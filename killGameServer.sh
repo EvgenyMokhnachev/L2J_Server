b8795aafd0868b0c41b621ac4ed06d26ce160133
evgenymokhnachev <evgeny.mokhnachev@softeq.com>
2017-10-26 10:38:35 +0300

    fix kill bash scripts

--- killGameServer.sh
+++ killGameServer.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-kill $(ps aux | grep 'l2jserver' | awk '{print $2}')
+kill $(ps aux | grep 'l2jserver' | awk '{print $2}') &&
+kill $(ps aux | grep 'GameServer_loop' | awk '{print $2}')
--- killLoginServer.sh
+++ killLoginServer.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-kill $(ps aux | grep 'l2jlogin.jar' | awk '{print $2}')
+kill $(ps aux | grep 'l2jlogin.jar' | awk '{print $2}') &&
+kill $(ps aux | grep 'LoginServer_loop' | awk '{print $2}')
