24a6d199c98dd61adbea8220287b852dcfe2c979
evgenymokhnachev <evgeny.mokhnachev@softeq.com>
2017-10-26 10:50:25 +0300

    fix kill scripts

--- killLoginServer.sh
+++ killLoginServer.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-kill $(ps aux | grep 'l2jlogin.jar' | awk '{print $2}') &&
-kill $(ps aux | grep 'LoginServer_loop' | awk '{print $2}')
+(kill $(ps aux | grep 'l2jlogin.jar' | awk '{print $2}')) || (echo Killed l2jlogin.jar) &&
+(kill $(ps aux | grep 'LoginServer_loop' | awk '{print $2}')) || (echo Killed LoginServer_loop)
